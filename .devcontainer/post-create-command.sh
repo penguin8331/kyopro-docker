7ebc6b1e4928ba424c8387b5a19459a601b831d6
penguin8331 <111721385+penguin8331@users.noreply.github.com>
2026-06-10 11:26:17 +0900

    Initial commit

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-completions zsh-syntax-highlighting)/' ~/.zshrc
+
+git submodule update --init
